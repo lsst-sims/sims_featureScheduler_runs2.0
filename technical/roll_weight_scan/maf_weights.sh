f0eacaa89ea6108c69300f1849e633819d8ff71e
Peter Yoachim <yoachim@uw.edu>
2021-07-19 16:35:16 -0700

    rolling weight

---
+++ technical/roll_weight_scan/maf_weights.sh
@@ -0,0 +1,13 @@
+scimaf_dir --db roll_weight_rs_0.1_v2.0_10yrs.db
+scimaf_dir --db roll_weight_rs_0.3_v2.0_10yrs.db
+scimaf_dir --db roll_weight_rs_0.5_v2.0_10yrs.db
+scimaf_dir --db roll_weight_rs_0.7_v2.0_10yrs.db
+scimaf_dir --db roll_weight_rs_0.9_v2.0_10yrs.db
+scimaf_dir --db roll_weight_rs_1.0_v2.0_10yrs.db
+glance_dir --db roll_weight_rs_0.1_v2.0_10yrs.db
+glance_dir --db roll_weight_rs_0.3_v2.0_10yrs.db
+glance_dir --db roll_weight_rs_0.5_v2.0_10yrs.db
+glance_dir --db roll_weight_rs_0.7_v2.0_10yrs.db
+glance_dir --db roll_weight_rs_0.9_v2.0_10yrs.db
+glance_dir --db roll_weight_rs_1.0_v2.0_10yrs.db
+
---
+++ technical/roll_weight_scan/roll_weights.sh
@@ -0,0 +1,6 @@
+python roll_weight.py --rolling_strength 1.0
+python roll_weight.py --rolling_strength 0.9
+python roll_weight.py --rolling_strength 0.7
+python roll_weight.py --rolling_strength 0.5
+python roll_weight.py --rolling_strength 0.3
+python roll_weight.py --rolling_strength 0.1
