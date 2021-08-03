864cf613da361e34ff713cda518e29432bd7c8f9
Michael Coughlin <michael.coughlin@ligo.org>
2021-08-03 06:09:32 -0700

    Add Peter's paired suggestion

--- baseline/baseline.py
+++ baseline/baseline.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import numpy as np
 import matplotlib.pylab as plt
 import healpy as hp
@@ -467,6 +469,7 @@ if __name__ == "__main__":
     parser.add_argument("--rolling_strength", type=float, default=0.9)
     parser.add_argument("--dbroot", type=str)
     parser.add_argument('--filters', help="filter distribution (default: u 0.07 g 0.09 r 0.22 i 0.22 z 0.20 y 0.20)")
+    parser.add_argument("--same_pairs",  action="store_true", default=False)
 
     args = parser.parse_args()
     survey_length = args.survey_length  # Days
@@ -546,9 +549,25 @@ if __name__ == "__main__":
     ddfs = generate_dd_surveys(nside=nside, nexp=nexp, detailers=details, euclid_detailers=euclid_detailers)
 
     greedy = gen_greedy_surveys(nside, nexp=nexp, footprints=footprints)
-    blobs = generate_blobs(nside, nexp=nexp, footprints=footprints)
-    twi_blobs = generate_twi_blobs(nside, nexp=nexp, footprints=footprints, wfd_footprint=wfd_footprint,
-                                   repeat_night_weight=repeat_night_weight)
+
+    if args.same_pairs:
+        filters_blobs=['u', 'g', 'r', 'i', 'z', 'y']
+        filters_twi=['r', 'i', 'z', 'y']
+        blobs = generate_blobs(nside, nexp=nexp, footprints=footprints,
+                               filter1s=filters_blobs,
+                               filter2s=filters_blobs)
+        twi_blobs = generate_twi_blobs(nside, nexp=nexp,
+                                       footprints=footprints,
+                                       wfd_footprint=wfd_footprint,
+                                       repeat_night_weight=repeat_night_weight,
+                                       filter1s=filters_twi,
+                                       filter2s=filters_twi)
+    else:
+        blobs = generate_blobs(nside, nexp=nexp, footprints=footprints)
+        twi_blobs = generate_twi_blobs(nside, nexp=nexp,
+                                       footprints=footprints,
+                                       wfd_footprint=wfd_footprint,
+                                       repeat_night_weight=repeat_night_weight)
     surveys = [ddfs, blobs, twi_blobs, greedy]
     run_sched(surveys, survey_length=survey_length, verbose=verbose,
               fileroot=os.path.join(outDir, fileroot+file_end), extra_info=extra_info,
