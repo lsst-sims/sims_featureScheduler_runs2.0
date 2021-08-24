0cd4fb762de1e93dcc8fc05ad609575200ef8e00
Peter Yoachim <yoachim@uw.edu>
2021-08-23 23:13:01 -0700

    typo

--- long_u/maf.sh
+++ long_u/maf.sh
@@ -1,4 +1,4 @@
-scimaf_dir --db long_u1_v2.0_0yrs.db
-scimaf_dir --db long_u2_v2.0_0yrs.db
-glance_dir --db long_u1_v2.0_0yrs.db
-glance_dir --db long_u2_v2.0_0yrs.db
+scimaf_dir --db long_u1_v2.0_10yrs.db
+scimaf_dir --db long_u2_v2.0_10yrs.db
+glance_dir --db long_u1_v2.0_10yrs.db
+glance_dir --db long_u2_v2.0_10yrs.db
