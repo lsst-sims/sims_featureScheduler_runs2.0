5e42436b60857b0dc34867d7b46de074caee2b74
Peter Yoachim <yoachim@uw.edu>
2021-08-26 12:42:15 -0700

    bluer

---
+++ bluer/bluer.sh
@@ -0,0 +1,2 @@
+python bluer.py --filter_balance_indx 0
+python bluer.py --filter_balance_indx 1
---
+++ bluer/maf.sh
@@ -0,0 +1,4 @@
+glance_dir --db bluer_indx0_v2.0_10yrs.db
+scimaf_dir --db bluer_indx0_v2.0_10yrs.db
+glance_dir --db bluer_indx1_v2.0_10yrs.db
+scimaf_dir --db bluer_indx1_v2.0_10yrs.db
