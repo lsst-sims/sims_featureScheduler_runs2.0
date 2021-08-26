03ad4a480d9a820819d19571ba94e4de3b44bfea
Peter Yoachim <yoachim@uw.edu>
2021-08-26 09:54:53 -0700

    rolling.sh

---
+++ rolling/rolling.sh
@@ -0,0 +1,6 @@
+python rolling.py --rolling_nslice 2 --rolling_strength 0.5
+python rolling.py --rolling_nslice 2 --rolling_strength 0.9
+python rolling.py --rolling_nslice 3 --rolling_strength 0.5
+python rolling.py --rolling_nslice 3 --rolling_strength 0.9
+python rolling.py --rolling_nslice 6 --rolling_strength 0.5
+python rolling.py --rolling_nslice 6 --rolling_strength 0.9
