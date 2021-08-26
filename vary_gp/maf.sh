b67c9adb222da3547cec8ab05f7d04b242cf6647
Peter Yoachim <yoachim@uw.edu>
2021-08-26 16:14:50 -0700

    vary gp

---
+++ vary_gp/maf.sh
@@ -0,0 +1,4 @@
+glance_dir --db vary_gp_nvgp116_v2.0_10yrs.db
+scimaf_dir --db vary_gp_nvgp116_v2.0_10yrs.db
+glance_dir --db vary_gp_nvgp348_v2.0_10yrs.db
+scimaf_dir --db vary_gp_nvgp348_v2.0_10yrs.db
---
+++ vary_gp/vary_gp.sh
@@ -0,0 +1,2 @@
+python vary_gp.py --nvis_gp 116
+python vary_gp.py --nvis_gp 348
