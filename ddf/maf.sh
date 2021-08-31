7393842db6a5c346c262f6d8e138537f8f01998b
Peter Yoachim <yoachim@uw.edu>
2021-08-31 15:41:13 -0700

    ddf start

---
+++ ddf/maf.sh
@@ -0,0 +1,4 @@
+glance_dir --db ddf_frac_ddf_per0.6_v2.0_0yrs.db
+scimaf_dir --db ddf_frac_ddf_per0.6_v2.0_0yrs.db
+glance_dir --db ddf_frac_ddf_per1.6_v2.0_0yrs.db
+scimaf_dir --db ddf_frac_ddf_per1.6_v2.0_0yrs.db
