c54e6809d7e3900f22eb3932734f0f12c1e17cf5
Peter Yoachim <yoachim@uw.edu>
2021-09-09 19:31:25 -0700

    rolling bulge

---
+++ rolling_bulge/maf.sh
@@ -0,0 +1,6 @@
+glance_dir --db rolling_bulge_ns2_rw0.5_v2.0_10yrs.db
+glance_dir --db rolling_bulge_ns2_rw0.9_v2.0_10yrs.db
+glance_dir --db rolling_bulge_ns2_rw0.8_v2.0_10yrs.db
+scimaf_dir --db rolling_bulge_ns2_rw0.5_v2.0_10yrs.db
+scimaf_dir --db rolling_bulge_ns2_rw0.9_v2.0_10yrs.db
+scimaf_dir --db rolling_bulge_ns2_rw0.8_v2.0_10yrs.db
---
+++ rolling_bulge/rolling_bulge.sh
@@ -0,0 +1,3 @@
+python rolling.py --rolling_nslice 2 --rolling_strength 0.5
+python rolling.py --rolling_nslice 2 --rolling_strength 0.9
+python rolling.py --rolling_nslice 2 --rolling_strength 0.8
