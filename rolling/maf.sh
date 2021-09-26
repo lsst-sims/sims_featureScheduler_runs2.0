45c1fbf7a71c73a194fe1dfa11e9b7e126bf2391
Peter Yoachim <yoachim@uw.edu>
2021-09-26 16:48:58 -0700

    no more 6-band here

--- rolling/maf.sh
+++ rolling/maf.sh
@@ -2,8 +2,6 @@ glance_dir --db rolling_ns2_rw0.5_v2.0_10yrs.db
 glance_dir --db rolling_ns2_rw0.9_v2.0_10yrs.db
 glance_dir --db rolling_ns3_rw0.5_v2.0_10yrs.db
 glance_dir --db rolling_ns3_rw0.9_v2.0_10yrs.db
-glance_dir --db rolling_ns6_rw0.5_v2.0_10yrs.db
-glance_dir --db rolling_ns6_rw0.9_v2.0_10yrs.db
 scimaf_dir --db rolling_ns2_rw0.5_v2.0_10yrs.db
 scimaf_dir --db rolling_ns2_rw0.9_v2.0_10yrs.db
 scimaf_dir --db rolling_ns3_rw0.5_v2.0_10yrs.db
