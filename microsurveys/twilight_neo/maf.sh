5e238c9e083870e963e6f8d207dab71b0e57c1b6
Peter Yoachim <yoachim@uw.edu>
2021-10-05 10:16:25 -0700

    twilight neo

---
+++ microsurveys/twilight_neo/maf.sh
@@ -0,0 +1,14 @@
+scimaf_dir --db twilight_neo_nightpattern1v2.0_10yrs.db
+scimaf_dir --db twilight_neo_nightpattern2v2.0_10yrs.db
+scimaf_dir --db twilight_neo_nightpattern3v2.0_10yrs.db
+scimaf_dir --db twilight_neo_nightpattern4v2.0_10yrs.db
+scimaf_dir --db twilight_neo_nightpattern5v2.0_10yrs.db
+scimaf_dir --db twilight_neo_nightpattern6v2.0_10yrs.db
+scimaf_dir --db twilight_neo_nightpattern7v2.0_10yrs.db
+glance_dir --db twilight_neo_nightpattern1v2.0_10yrs.db
+glance_dir --db twilight_neo_nightpattern2v2.0_10yrs.db
+glance_dir --db twilight_neo_nightpattern3v2.0_10yrs.db
+glance_dir --db twilight_neo_nightpattern4v2.0_10yrs.db
+glance_dir --db twilight_neo_nightpattern5v2.0_10yrs.db
+glance_dir --db twilight_neo_nightpattern6v2.0_10yrs.db
+glance_dir --db twilight_neo_nightpattern7v2.0_10yrs.db
---
+++ microsurveys/twilight_neo/twi_neo.sh
@@ -0,0 +1,7 @@
+python twilight_neo.py --night_pattern 1
+python twilight_neo.py --night_pattern 2
+python twilight_neo.py --night_pattern 3
+python twilight_neo.py --night_pattern 4
+python twilight_neo.py --night_pattern 5
+python twilight_neo.py --night_pattern 6
+python twilight_neo.py --night_pattern 7
