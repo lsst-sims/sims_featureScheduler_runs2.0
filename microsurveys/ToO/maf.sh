6d592072380f3bc231c3298b3a3f71856257d77d
Peter Yoachim <yoachim@uw.edu>
2021-10-15 17:15:47 -0700

    sh files

---
+++ microsurveys/ToO/maf.sh
@@ -0,0 +1,6 @@
+glance_dir --db too_rate10_v2.0_10yrs.db
+glance_dir --db too_rate10_v2.0_50yrs.db
+glance_dir --db too_rate10_v2.0_100yrs.db
+scimaf_dir --db too_rate10_v2.0_10yrs.db
+scimaf_dir --db too_rate10_v2.0_50yrs.db
+scimaf_dir --db too_rate10_v2.0_100yrs.db
---
+++ microsurveys/ToO/too.sh
@@ -0,0 +1,3 @@
+python too.py --too_rate 10
+python too.py --too_rate 50
+python too.py --too_rate 100
