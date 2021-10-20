db8ad7b1d6ec44a4437978bd8e2e7710ea860526
Peter Yoachim <yoachim@uw.edu>
2021-10-20 13:32:53 -0700

    for devl

---
+++ presto_half/README.md
@@ -0,0 +1,1 @@
+Run presto but only let it execute every-other night so the rest of the survey can keep up.
---
+++ presto_half/presto_half.sh
@@ -0,0 +1,12 @@
+python presto_half.py --long_gap 1.5
+python presto_half.py --long_gap 2.0
+python presto_half.py --long_gap 2.5
+python presto_half.py --long_gap 3.0
+python presto_half.py --long_gap 3.5
+python presto_half.py --long_gap 4.0
+python presto_half.py --long_gap 1.5 --mix_pairs
+python presto_half.py --long_gap 2.0 --mix_pairs
+python presto_half.py --long_gap 2.5 --mix_pairs
+python presto_half.py --long_gap 3.0 --mix_pairs
+python presto_half.py --long_gap 3.5 --mix_pairs
+python presto_half.py --long_gap 4.0 --mix_pairs
