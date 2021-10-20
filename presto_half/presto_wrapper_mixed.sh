9ac0ef3d3753776777ac4c18e99251f704fc34b6
Peter Yoachim <yoachim@uw.edu>
2021-10-20 13:45:45 -0700

    more bash

--- presto_half/presto_wrapper_mixed.sh
+++ presto_half/presto_wrapper_mixed.sh
@@ -4,6 +4,6 @@
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate rubin
 
-root_dir = "/scratch/yoachim/git_repos/sims_featureScheduler_runs2.0/presto_half/"
+root_dir="/scratch/yoachim/git_repos/sims_featureScheduler_runs2.0/presto_half/"
 
 python ${root_dir}presto_half.py --outDir ${root_dir} $1 --mix_pairs
