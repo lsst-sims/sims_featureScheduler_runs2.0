9bb45fcc571df63bdd10464792455564c0a4f1f7
Peter Yoachim <yoachim@uw.edu>
2021-10-27 13:58:52 -0700

    too ss

--- microsurveys/ToO/maf.sh
+++ microsurveys/ToO/maf.sh
@@ -1,6 +1,6 @@
 glance_dir --db too_rate10_v2.0_10yrs.db
 glance_dir --db too_rate10_v2.0_50yrs.db
-glance_dir --db too_rate10_v2.0_100yrs.db
+glance_dir --db baseline_v2.0_10yrs.db
 scimaf_dir --db too_rate10_v2.0_10yrs.db
 scimaf_dir --db too_rate10_v2.0_50yrs.db
-scimaf_dir --db too_rate10_v2.0_100yrs.db
+scimaf_dir --db baseline_v2.0_10yrs.db
